0d1fbd49d06b009fd2ae87ab0468f6eda89af6e2
VaiDik-cyber <patelvaidik56@gmail.com>
2023-12-16 17:08:37 +0530

    Navbar , Footer and Pages created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "../src/Components/layouts/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../src/Pages/Home";
+import About from "../src/Pages/About";
+import Footer from "./Components/layouts/Footer";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container px-3 pb-12 mx-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
